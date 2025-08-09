fe97d50d378e7c4d17ddea0402b8f67f671af1e5
KWaloen <k.waloen91@hotmail.com>
2025-08-09 16:15:28 +0200

    fix up volunteer page and add navbar

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -4,28 +4,14 @@ import styles from './navbar.module.css'
 
 export default function NavBar() {
   return (
-    <div className= {styles.navbar}>
-      <div className= {styles.navbarleft}>
-  
-      </div>
-
-      <div className= {styles.navbarright}>
-        <div className = {styles.dropdown}>
-        <Image
-          className={styles.LOGO_CREAM}
-          src="/navbar/hamburger.png"
-          alt="dropdown"
-          width={30}
-          height={20}
-        />
-          <div className = {styles.dropdownmenu}>
-            <Link href="/">Home</Link>
-            <Link href="/artists">Artists</Link>
-            <Link href="/events">Events</Link>
-            <Link href="/volunteer">Volunteer</Link>
-            <Link href="/faqs">FAQS</Link>
-          </div>
-      </div>
+    <div className={styles.navbar}>
+      
+      <div className={styles.navbarright}>
+        <Link href="/">Home</Link>
+        <Link href="/artists">Artists</Link>
+        <Link href="/events">Events</Link>
+        <Link href="/volunteer">Volunteer</Link>
+        <Link href="/faqs">FAQS</Link>
       </div>
     </div>
   );
--- src/app/volunteer/page.js
+++ src/app/volunteer/page.js
@@ -11,18 +11,6 @@ export default function Home() {
 
       <div className={styles.page}>
         <NavBar />
-
-        <Image
-          className={styles.concert}
-          src="/volunteer/concert.png"
-          alt="concert"
-          width={1000}
-          height={50}
-        />
-
-        <div className={styles.volunteerText}>
-          VOLUNTEERS
-        </div>
       </div>
 
       <div className={styles.pageContent}>
@@ -38,99 +26,8 @@ export default function Home() {
         <div className={styles.tramFestival}>
           THE TRAM FESTIVAL IS A VOLUNTARY INITIATIVE WHERE WE WANT TO HOLD A MUSIC FESTIVAL ON THE HISTORIC TRAM THAT RUNS FROM CAFE OPERA TO THE TECHNICAL MUSEUM AT MØHLENPRIS
         </div>
-
-        <div className={styles.oval}>
-          BECOME A VOLUNTEER
-        </div>
-
-        <div className={styles.imageGrid}>
-          <Image
-            className={styles.trikk}
-            src="/volunteer/blackTram.gif"
-            alt="trikk"
-            width={150}
-            height={100}
-          />
-
-          <Image
-            className={styles.trikk}
-            src="/volunteer/tram1.png"
-            alt="tram1"
-            width={100}
-            height={100}
-          />
-        </div>
-
-        <Image
-          className={styles.volunteerImages}
-          src="/volunteer/musikkpåskinner.png"
-          alt="banner"
-          width={800}
-          height={30}
-        />
-
-        <div className={styles.age}>
-          NOTE: YOU MUST BE 18 YEARS OR OLDER TO VOLUNTEER
-        </div>
-
+        
       </div>
-
-      <div className={styles.images}>
-        <Image
-          className={styles.banner}
-          src="/volunteer/banner.png"
-          alt="banner"
-          width={2000}
-          height={100}
-        />
-
-        <Image
-          className={styles.tramShed}
-          src="/volunteer/tramShed.png"
-          alt="banner"
-          width={1000}
-          height={100}
-        />
-
-
-        <Image
-          className={styles.banner}
-          src="/volunteer/bannerend.png"
-          alt="banner"
-          width={2000}
-          height={100}
-        />
-
-        <Image
-          className={styles.volunteerImages}
-          src="/volunteer/musikkpåskinner.png"
-          alt="banner"
-          width={800}
-          height={30}
-        />
-
-        <Image
-          className={styles.volunteerImages}
-          src="/volunteer/musicBanner.png"
-          alt="banner"
-          width={800}
-          height={30}
-        />
-
-        <div className={styles.roles}>
-          WE ARE MADE UP OF DIFFERENT GROUPS COVERING DIFFERENT AREAS NEEDED WITHIN THE FESTIVAL
-        </div>
-
-        <div className={styles.apply}>
-          APPLY NOW AND HELP MAKE THIS YEAR'S TRAM MUSIC FESTIVAL THE MOST EPIC ONE YET! SPOTS FILL UP QUICKLY, SO HOP ABOARD TODAY!
-        </div>
-
-        <div className={styles.oval}>
-          BECOME A VOLUNTEER
-        </div>
-      </div>
-
-      <Subscribe />
 
       <Footer />
     </div>
