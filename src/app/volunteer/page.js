458814539ac19dcb562c3b9efce015d9b599ca7c
KWaloen <k.waloen91@hotmail.com>
2025-08-09 17:57:40 +0200

    fix spacing on volunteer page and add link to join discord page

--- src/app/volunteer/page.js
+++ src/app/volunteer/page.js
@@ -1,9 +1,9 @@
 
 import Image from 'next/image'
+import Link from "next/link"
 import styles from './volunteer.module.css'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import Subscribe from '../components/Subscribe'
 
 export default function Home() {
   return (
@@ -26,7 +26,23 @@ export default function Home() {
         <div className={styles.tramFestival}>
           THE TRAM FESTIVAL IS A VOLUNTARY INITIATIVE WHERE WE WANT TO HOLD A MUSIC FESTIVAL ON THE HISTORIC TRAM THAT RUNS FROM CAFE OPERA TO THE TECHNICAL MUSEUM AT MØHLENPRIS
         </div>
-        
+
+        <div className={styles.tramFestival}>
+          <Link href="https://discord.gg/2Zb9xaUmjU">
+            <Image
+              src={"/volunteer/Discord-Symbol-Blurple.png"}
+              alt="logo"
+              width={40}
+              height={25}
+            />
+            <div>
+              JOIN THE DISCORD
+            </div>
+          </Link>
+        </div>
+
+
+
       </div>
 
       <Footer />
