4d4d6a94d75b1c76d6598c9f1c9d7183c649b8df
Illimar Rekand <illimar.rekand@uib.no>
2025-08-09 17:23:50 +0200

    Add some info

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -40,8 +40,24 @@ export default function Home() {
           </div>
 
           <div className={styles.tramText}>
-            <div className={styles.sequence}>
-              VI STARTER MED CAFÉ OPERA PÅ ENGEN OG AVSLUTTER MED TEKNISK MUSEUM PÅ MØHLENPRIS
+            <div className={styles.boxContainer}>
+            <div className={styles.box}>
+            <div className={styles.getongetoff}>
+              BILLETTINFORMASJON
+            </div>
+              På denne siden kommer informasjon om hvordan man kjøper billetter, samt praktisk informasjon om festivalen.
+            </div>
+            <div className={styles.boxContainer}>
+              <div className={styles.box}>
+              <div className={styles.getongetoff}>
+                Hva er Trikkefestivalen?
+              </div>
+              I år (2025) arrangerer vi for første gang Trikkefestivalen, en lokal musikkfestival hvor vi kombinerer den historiske trikken med moderne musikk! Seks lokale, unge artister vil i løpet av en dag fremføre musikk mens trikken beveger seg til og fra Café Opera over Høyden til Teknisk Museum på Møhlenpris
+              <br />
+              <br />
+              Festivalen drives med støtte fra Bergen Kommune og Studenradioen i Bergen. Vi har også partnerskap med Bergen Elektriske Sporvei, som drifter trikken og Café Opera og Nobel Bopel.
+              </div>
+            </div>
             </div>
           </div>
         </div>
